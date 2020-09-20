6b3b6315b7d06bf71572940dd4a4bcd4f63e32a6
ARIEL_GODDAMMIT <pleasantbowel5yndrome@gmail.com>
2020-09-20 11:57:42 -0700

    added the builtins

--- owlsh.c
+++ owlsh.c
@@ -8,7 +8,9 @@
 #include <ctype.h> // for isspace
 
 
-const int DEBUG = 1;
+const int DEBUG = 0;
+
+
 // trim takes a string and returns the string without any whitespace on the ends
 // because its a return value, its on the heap, so it needs to be free'd
 char* trim(char* line) {
@@ -68,19 +70,110 @@ char** parse(char *line) {
     }
     return args;
 }
-int handle(int argc, char **argv) {
+int handle(int argc, char **argv, char *PATH, char *prompt) {
 	int i = 0;
-
+	char* token;
 	printf("argc is %i",argc);
-	for(; i < argc + 1; i++) {
+
+	if (argc = 0)
+	{
+		return -1;
+	}
+
+	//for(; i < argc + 1; i++) {
+		token = argv[0];
+
+		char exit_str[] = "exit";
+		if (strcmp(token, exit_str) == 0) {
+			if (DEBUG) printf ("sick dude that says exit\n");
+			exit(0);
+		}
+
+		char *cd = "cd"; // I want to try all the different ways of making strings
+		if (strcmp(token, cd) == 0) {
+			if (DEBUG) printf ("sick dude that says cd\n");
+
+			//execute chdir with the next token
+
+			//first, make sure the next token exists
+			//token = strtok(NULL, delim);
+			if (argc > 1) {
+				token = argv[1];
+			}
+			else {
+				char s[100];
+				if (DEBUG) printf("current directory is: %s\n", getcwd(s, 100));
+				if (DEBUG) printf("current token is: ~<3%s\n~<3", token);
+				int zeroForSuccess = chdir(token);
+				if (zeroForSuccess == 0)
+				{
+					if (DEBUG) printf("nice, your cd command worked\n");
+					// WE HAVE TO VALGRIND THIS: DEF A MEMORY LEAK HERE
+					//char new_prompt[260]; //that's the max path length for windows
+					char *old_prompt = (char*) calloc(261, sizeof(char));
+					strcpy(old_prompt, prompt);
+
+					//clearing prompt
+					memset (prompt, 0, sizeof(prompt));
+
+					strcpy(prompt, token);
+					strcat(prompt,"/");
+					strcat(prompt, old_prompt);
+
+					free(old_prompt);
+					if (DEBUG) printf( "new prompt is %s\n", prompt);
+
+
+
+				}
+				else {
+					printf("that's not a directory :(\n");
+				}
+				if (DEBUG) printf("NEW directory is: %s\n", getcwd(s, 100));
+			}
+		}
+
+		
+
+		char *path_str = "path";
+
+		if (strcmp(token, path_str) == 0) {
+			if (DEBUG) printf ("sick dude that says path\n");
+			
+			if (argc == 1) {
+				printf("%s\n",PATH);
+			}
+			else {
+				int i = 1;
+				for(; i < argc; i++){
+					strcat(PATH, " ");
+					strcat(PATH, token);
+				}
+			}
+
+		}
+
+
+
 		printf("%s\n", argv[i]);
 		// fprintf(stderr, "%s\n", argv[i]);
 		//printf("%s\n", argv[i]);
 		//fprintf(stderr, "%s\n", argv[i]);
-	}
+	//}
 }
 int main (int argc, char** argv)
 {
+	//creating prompt and PATH char*
+	char *path_initially = " ";
+	char *PATH = (char*) calloc(400, sizeof(char));
+	strcpy(PATH, path_initially);
+
+	char* name_of_prompt = "owlsh> ";
+	char *prompt = (char*) calloc(261, sizeof(char));
+	strcpy(prompt, name_of_prompt);
+
+
+
 	char error_message[30] = "An error has occurred\n";
 	int BUFFER_SIZE = sysconf(ARG_MAX);
 	FILE *fp;
@@ -100,9 +193,7 @@ int main (int argc, char** argv)
 		free(line);
 		exit(1);
 	} else {
-		char* name_of_prompt = "owlsh> ";
-		char *prompt = (char*) calloc(261, sizeof(char));
-		strcpy(prompt, name_of_prompt);
+		
 		printf("%s",prompt);
 		while((nread = getline(&line, &len, fp)) != -1) {
 			char *cmd, *out;
@@ -115,7 +206,7 @@ int main (int argc, char** argv)
 				freopen(trim(out), "w+", stderr);
 			}
 			char **args = parse(line);
-			handle(count_spaces_in_line(line), args);
+			handle(count_spaces_in_line(line), args, PATH, prompt);
 			//printf("%s",prompt);
 			free(args);
 		}
