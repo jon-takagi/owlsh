914c323e6ea2ed006241766d904ae1c734c61463
Jon Takagi <jontakagi@gyoza.reed.edu>
2020-09-19 17:07:15 -0700

    oh thats how that works

--- parser.c
+++ parser.c
@@ -17,20 +17,17 @@ int count_spaces_in_line(char line[]) {
     return count;
 }
 char** parse(char *line) {
-    char line_arr[sizeof(line)];
+    char line_arr[strlen(line)];
     strcpy(line_arr, line);
     char *token, *reenterant;
     int spaces = count_spaces_in_line(line);
-    char **args = (char **)calloc(spaces + 1, 4096*32);
+    // char *args[spaces];
+    char **args = (char **)malloc((spaces + 1) * 8);
     // Note that args[i][j] is same as *(*(args+i)+j)
     int i = 0;
     token = strtok_r(line_arr, " ", &reenterant);
     while(token != NULL) {
-        // strcpy((char*)(*(args + 4096*32*i)), token);
-        // *(args + 4096*32*i) = *token;
-        // for(int j = 0; j < sizeof(token); j++) {
-        //     *(args + 4096*32*i + j) = token[j];
-        // }
+        args[i] = (char*)malloc(strlen(line) * sizeof(char));
         strcpy(args[i], token);
         i += 1;
         token = strtok_r(NULL, " ", &reenterant);
@@ -40,16 +37,14 @@ char** parse(char *line) {
 
 int main() {
     char *line, **args;
-    // size_t len = 10;
-    // getline(&line, &len, stdin);
     line = "ls -l ~/docs";
     printf("there are %d spaces in the line\n", count_spaces_in_line(line));
     args = parse(line);
-    // printf("args has: %ld items\n", sizeof(args));
-    // for(int i = 0; i < count_spaces_in_line(line) + 1; i++) {
-    //     printf("%s\n", args[i]);
-    //     free(args[i]);
-    // }
-    // free(args);
+    printf("args has: %ld items\n", sizeof(args));
+    for(int i = 0; i < count_spaces_in_line(line) + 1; i++) {
+        printf("%s\n", args[i]);
+        free(args[i]);
+    }
+    free(args);
     return 0;
 }
